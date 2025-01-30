722792a7af4ef94d21072be8bd8dc864f6da2e41
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-01-30 11:25:07 +0100

    MAJOR CHANGE: removed deprecated code

--- src/mausy5043_common/funfile.py
+++ src/mausy5043_common/funfile.py
@@ -7,8 +7,6 @@
 """Provide file operation functions."""
 
 import os
-import syslog
-import warnings
 
 
 def cat(file_name: str) -> str:
@@ -25,30 +23,3 @@ def cat(file_name: str) -> str:
         with open(file_name, encoding="utf-8") as file_stream:
             contents = file_stream.read().strip("\n")
     return contents
-
-
-def syslog_trace(trace: str, logerr: int, out2console: bool = False) -> None:
-    """Log a (multi-line) message to syslog.
-
-    Initialise with a call to
-    syslog.openlog(ident=<string>, facility=<syslog.facility>)
-
-    Args:
-        trace (str): Text to send to log
-        logerr (int): syslog errornumber
-        out2console (bool): If True, will also print the 'trace' to the screen
-
-    Returns:
-        None
-    """
-    warnings.warn(
-        "This function is deprecated. Use logging instead.",
-        category=DeprecationWarning,
-        stacklevel=2,
-    )
-    log_lines: list[str] = trace.split("\n")
-    for line in log_lines:
-        if line and logerr:
-            syslog.syslog(logerr, line)
-        if line and out2console:
-            print(line)
